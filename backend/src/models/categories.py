76409ed3793da570676f2c70ccc70e13152eb595
stepzak <stepanzakatin@gmail.com>
2026-05-05 20:58:04 +0300

    add type to category

--- backend/src/models/categories.py
+++ backend/src/models/categories.py
@@ -1,3 +1,4 @@
+import enum
 from uuid import UUID
 
 from sqlalchemy import ForeignKey, String, Boolean
@@ -5,6 +6,9 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from backend.src.models.base_class import Base
 
+class CategoryType(enum.Enum):
+    INCOME = 'income'
+    OUTCOME = 'outcome'
 
 class Category(Base):
     __tablename__ = 'categories'
@@ -12,4 +16,5 @@ class Category(Base):
     user_id: Mapped[UUID] = mapped_column(ForeignKey('users.id'))
     name: Mapped[str] = mapped_column(String(100))
     emoji: Mapped[str] = mapped_column(String(20))
+    type: Mapped[CategoryType]
     is_deleted: Mapped[bool] = mapped_column(Boolean, default = False)
--- backend/src/schemas/category.py
+++ backend/src/schemas/category.py
@@ -2,12 +2,16 @@ from uuid import UUID
 
 from pydantic import BaseModel
 
+from backend.src.models.categories import CategoryType
+
 
 class CategoryRecord(BaseModel):
     id: UUID
     name: str
     emoji: str
+    type: CategoryType
 
 class CategoryUpdate(BaseModel):
     name: str
     emoji: str
+    type: CategoryType
