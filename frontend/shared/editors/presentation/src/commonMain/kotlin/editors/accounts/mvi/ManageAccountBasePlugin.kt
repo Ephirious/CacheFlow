16c5b154867a6b2d4b97575268ac9090966e5964
Matashkov Artyom <artemker667@gmail.com>
2026-06-02 07:21:21 +0300

    hot-fix: fix title check for accounts

--- frontend/shared/editors/presentation/src/commonMain/kotlin/editors/accounts/mvi/ManageAccountBasePlugin.kt
+++ frontend/shared/editors/presentation/src/commonMain/kotlin/editors/accounts/mvi/ManageAccountBasePlugin.kt
@@ -39,11 +39,15 @@ fun <S : MVIState, I : ManageAccountBaseIntent, A : MVIAction, F : ManageAccount
                     }
 
                     is ManageAccountBaseIntent.ChangedTitle -> {
-                        @Suppress("UNCHECKED_CAST")
-                        (currentForm.copyBase(title = baseIntent.title)
-                                as ManageAccountFormBaseState<ManageAccountFormBaseValidationErrors>)
-                            .validated(ManageAccountFormBaseValidationFields.title)
-
+                        when (val f = currentForm.copyBase(title = baseIntent.title)) {
+                            is CreateAccountFormState -> f.validated(CreateAccountFormValidationFields.title)
+                            else -> {
+                                @Suppress("UNCHECKED_CAST")
+                                (f as ManageAccountFormBaseState<ManageAccountFormBaseValidationErrors>).validated(
+                                    ManageAccountFormBaseValidationFields.title
+                                )
+                            }
+                        }
                     }
 
                     ManageAccountBaseIntent.ClickedTryAgain -> currentForm
